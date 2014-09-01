39c450813b6b07b3423c8b1f07b6052df57c0551
NonWonderDog <nonwonderdog@gmail.com>
2014-08-31 22:38:05 -0400

    Add plugin update script

---
+++ updatePlugins.sh
@@ -0,0 +1,14 @@
+git subtree pull --prefix bundle/vim-pathogen git@github.com:tpope/vim-pathogen.git master --squash
+git subtree pull --prefix bundle/matchit git@github.com:tmhedberg/matchit.git master --squash
+git subtree pull --prefix bundle/vim-speeddating git@github.com:tpope/vim-speeddating.git master --squash
+git subtree pull --prefix bundle/vim-surround git@github.com:tpope/vim-surround.git master --squash
+git subtree pull --prefix bundle/vim-repeat git@github.com:tpope/vim-repeat.git master --squash
+git subtree pull --prefix bundle/vim-misc git@github.com:xolox/vim-misc.git master --squash
+git subtree pull --prefix bundle/vim-shell git@github.com:xolox/vim-shell.git master --squash
+git subtree pull --prefix bundle/vim-easytags git@github.com:xolox/vim-easytags.git master --squash
+git subtree pull --prefix bundle/vim-reload git@github.com:xolox/vim-reload.git master --squash
+git subtree pull --prefix bundle/tagbar git@github.com:majutsushi/tagbar.git master --squash
+git subtree pull --prefix bundle/ultisnips git@github.com:SirVer/ultisnips.git master --squash
+git subtree pull --prefix bundle/vim-snippets git@github.com:honza/vim-snippets.git master --squash
+git subtree pull --prefix bundle/tcomment_vim git@github.com:tomtom/tcomment_vim.git master --squash
+git subtree pull --prefix bundle/vim-hilinks git@github.com:kergoth/vim-hilinks.git master --squash
